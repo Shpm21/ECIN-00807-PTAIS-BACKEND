746bfe042a5d7b7d6f40fc910650eae7beba2853
Shpm21 <galleguilloscristian2@gmail.com>
2022-08-03 21:44:54 -0400

    new router request

--- src/routes/request.js
+++ src/routes/request.js
@@ -4,5 +4,8 @@ const RequestController = require('../controllers/request');
 const { validateTokenAuthentication, validateRut } = require('../middlewares/middlewares');
 
 router.get('/request/:rut', validateRut, validateTokenAuthentication, RequestController.getCoursesAvailable);
-
+router.get('/coursesStudent/', RequestController.getCoursesStudent);
+router.get('/prerequisites/', RequestController.getPrerequisites);
+router.get('/levelStudent/', RequestController.getLevelStudent);
+router.get('/averageApproved/', RequestController.getAverageApproved);
 module.exports = router;
