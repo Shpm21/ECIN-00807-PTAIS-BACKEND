b6bfbbcc03561b5144a9ae27e33eeb9c5895ad35
Shpm21 <galleguilloscristian2@gmail.com>
2022-08-04 21:27:30 -0400

    modify request getCourseAvailable

--- src/controllers/request.js
+++ src/controllers/request.js
@@ -1,9 +1,21 @@
 const { getSemesterStudent } = require('../querys/algorithm');
+const { getAverageApproved } = require('../querys/getInformation');
 
 exports.getCoursesAvailable = async (req, res) => {
     try {
+        const rut = req.body.rutStudent;
+        const isAverageApproval = req.body.isAverageApproval;
+        const response = await getSemesterStudent(rut, isAverageApproval);
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+};
+
+exports.getAverageApproval = async (req, res)=> {
+    try {
         const rut = req.params.rut;
-        response = await getSemesterStudent(rut);
+        const response = await getAverageApproved(rut);
         return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json(err);
