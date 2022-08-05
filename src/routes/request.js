0a587b488965c87f9181b76613bfed5f6b246545
Shpm21 <galleguilloscristian2@gmail.com>
2022-08-04 21:26:09 -0400

    create a new route from request

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,5 +3,6 @@ const router = Router();
 const RequestController = require('../controllers/request');
 const { validateTokenAuthentication, validateRut } = require('../middlewares/middlewares');
 
-router.get('/request/:rut', validateRut, validateTokenAuthentication, RequestController.getCoursesAvailable);
+router.post('/request/', validateTokenAuthentication, RequestController.getCoursesAvailable);
+router.get('/averageapproval/:rut', validateRut, validateTokenAuthentication, RequestController.getAverageApproval);
 module.exports = router;
