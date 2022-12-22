29dadd2d50d0c58b968d53ff0eee8c7b94d404d3
Shpm21 <galleguilloscristian2@gmail.com>
2022-12-21 22:58:52 -0300

    add jwt verification

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,53 +1,55 @@
 require("dotenv").config();
-
+const jwt = require("jsonwebtoken");
 
 const isRutValid = (rut) => {
-    if (rut.search('.') != -1 || rut.search('-') != -1)
-        return true;
-    return false;
-}
+  if (rut.search(".") != -1 || rut.search("-") != -1) return true;
+  return false;
+};
 
 exports.validateTokenAuthentication = (req, res, next) => {
-    try {
-        const authorization = req.get('authorization');
-        if (!authorization)
-            return res.status(401).json({message: 'Necesitas autorización'});
-        const token = authorization.split(' ').length == 2 ? authorization.split(' ')[1] : null;
+  try {
+    const authorization = req.get("authorization");
+    if (!authorization)
+      return res.status(401).json({ message: "Necesitas autorización" });
+    const token =
+      authorization.split(" ").length == 2 ? authorization.split(" ")[1] : null;
 
-        if (!token)
-            return res.status(401).json({message: 'Necesitas autorización'});
-        next();
-    } catch (err) {
-        return res.status(401).json({message: 'Necesitas autorización'});
-    }
-}
+    if (!token)
+      return res.status(401).json({ message: "Necesitas autorización" });
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decodedToken)
+      return res.status(401).json({ message: "Necesitas autorización" });
+    req.user = decodedToken;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Necesitas autorización" });
+  }
+};
 
 exports.validateRutBody = (req, res, next) => {
-    try {
-        if (isRutValid(req.body.rutStudent))
-            return next();
-        return res.status(404).json({message: 'Rut no valido'});
-    } catch (err) {
-        return res.status(500).json({message: 'Error en el servidor'});
-    }
-}
+  try {
+    if (isRutValid(req.body.rutStudent)) return next();
+    return res.status(404).json({ message: "Rut no valido" });
+  } catch (err) {
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
 
 exports.validateRut = (req, res, next) => {
-    try {        
-        if (isRutValid(req.params.rut))
-            return next();
-        return res.status(404).json({message: 'Rut no valido'});
-    } catch (err) {
-        return res.status(500).json({message: 'Error en el servidor'});
-    }
-}
+  try {
+    if (isRutValid(req.params.rut)) return next();
+    return res.status(404).json({ message: "Rut no valido" });
+  } catch (err) {
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
 
 exports.validateRutLogin = (req, res, next) => {
-    try {
-        if (isRutValid(req.body.rutStudent))
-            return next();
-        return res.status(404).json({message: 'Rut no valido'});
-    } catch (err) {
-        return res.status(500).json({message: 'Error en el servidor'});
-    }
-}
+  try {
+    if (isRutValid(req.body.rutStudent)) return next();
+    return res.status(404).json({ message: "Rut no valido" });
+  } catch (err) {
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
+};
